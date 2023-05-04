c0cc8e22c2222098d9e5cc4890ac64e6e3982b38
Luis Guedes <luisguedes.dev@gmail.com>
2023-05-04 11:55:56 -0300

    Concluido task SPT-12 Melhorias de segurança e desempenho do Backend

--- index.js
+++ index.js
@@ -3,11 +3,29 @@ const cors = require("cors");
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const helmet = require("helmet");
+const session = require('express-session');
+const compression = require('compression')
+
 
 // Configuração do App
 const app = express();
 app.use(express.json()); // Possibilitar transitar dados usando JSON
 app.use(morgan("dev"));
+
+// Melhorias de Segurança
+// Configuração do Helmet
+app.use(helmet());
+app.disable("x-powered-by");
+
+
+// Mascarando cookies
+app.set('trust proxy', 1) // trust first proxy
+app.use( session({
+   secret : 's3Cur3',
+   name : 'sessionId',
+  })
+);
 
 // Configurações de acesso
 app.use(cors({ origin: "http://localhost:3000" }));
@@ -16,6 +34,11 @@ app.use(cors({ origin: "http://localhost:3000" }));
 const { connection, authenticate } = require("./database/database");
 authenticate(connection); // efetivar a conexão
 
+
+// Melhoria de Performance
+// Implementação do gzip
+app.use(compression());
+
 // Definição de Rotas
 const rotasClientes = require("./routes/clientes");
 const rotasPets = require("./routes/pets");
@@ -23,6 +46,7 @@ const rotasProdutos = require("./routes/produtos");
 const rotasServicos = require("./routes/servicos");
 const rotasAgendamentos = require("./routes/agendamentos");
 const rotasPedidos = require("./routes/pedidos");
+const rotashealthchecker = require("./routes/healthchecker");
 
 // Juntar ao app as rotas dos arquivos
 app.use(rotasClientes); // Configurar o grupo de rotas no app
@@ -31,6 +55,7 @@ app.use(rotasProdutos);
 app.use(rotasServicos);
 app.use(rotasAgendamentos);
 app.use(rotasPedidos);
+app.use(rotashealthchecker);
 
 // Escuta de eventos (listen)
 app.listen(3001, () => {
